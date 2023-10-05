fb76c4429ea0619993618168793c0cea000e43b6
gopancakes <samuelfdove@gmail.com>
2023-10-04 19:18:00 -0500

    adding other moves

--- tests/testing_pseudo_moves.cpp
+++ tests/testing_pseudo_moves.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
   std::string line;
   std::ifstream FENfile("test_files/pseudoMoves.txt");
-  freopen("wrong_pseudo_moves.tst","w",stdout);
+  freopen("wrong_pseudo_moves.txt","w",stdout);
 
   if (FENfile.is_open()) {
     int linenum = 0;
@@ -36,73 +36,85 @@ int main() {
           }
           break;
         case 3:
-          // if (line != b.GetwBishop().GetBoard().to_string()) {
-          //   cout << "White Bishop moves are incorrect for FEN: " << FEN << endl;
-          //   cout << "File bitset: " << line << endl << "Board Bitset: " << b.GetwBishop().GetBoard().to_string() << endl;
-          // }
+          if (line != b.wBishopMoves().to_string()) {
+            cout << "White Bishop moves are incorrect for FEN: " << FEN << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.wBishopMoves() << endl;
+            PrintBitSet(b.wBishopMoves());
+          }
           break;
+
         case 4:
-          // if (line != b.GetwRook().GetBoard().to_string()) {
-          //   cout << "White Rook moves are incorrect for FEN: " << FEN << endl;
-          //   cout << "File bitset: " << line << endl << "Board Bitset: " << b.GetwRook().GetBoard().to_string() << endl;
-          // }
+          if (line != b.wRookMoves().to_string()) {
+            cout << "White Rook moves are incorrect for FEN: " << FEN << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.wRookMoves() << endl;
+            PrintBitSet(b.wRookMoves());
+          }
           break;
         case 5:
-          // if (line != b.GetwQueen().GetBoard().to_string()) {
-          //   cout << "White Queen moves are incorrect for FEN: " << FEN << endl;
-          //   cout << "File bitset: " << line << endl << "Board Bitset: " << b.GetwQueen().GetBoard().to_string() << endl;
-          // }
+          if (line != b.wQueenMoves().to_string()) {
+            cout << "White Queen moves are incorrect for FEN: " << FEN << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.wQueenMoves() << endl;
+            PrintBitSet(b.wQueenMoves());
+          }
           break;
+
         case 6:
           if (line != b.wKingMoves().to_string()) {
             cout << "White King moves are incorrect for FEN: " << FEN << endl;
-            cout << "File bitset:  " << line << endl << "Board Bitset: " << b.wKingMoves().to_string() << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.wKingMoves() << endl;
             PrintBitSet(b.wKingMoves());
           }
           break;
+
         case 7:
-          // Black pieces
           if (line != b.bPawnMoves().to_string()) {
             cout << "Black Pawn moves are incorrect for FEN: " << FEN << endl;
-            cout << "File bitset:  " << line << endl << "Board Bitset: " << b.bPawnMoves().to_string() << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.bPawnMoves() << endl;
             PrintBitSet(b.bPawnMoves());
           }
           break;
+
         case 8:
           if (line != b.bKnightMoves().to_string()) {
             cout << "Black Knight moves are incorrect for FEN: " << FEN << endl;
-            cout << "File bitset:  " << line << endl << "Board Bitset: " << b.bKnightMoves().to_string() << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.bKnightMoves() << endl;
             PrintBitSet(b.bKnightMoves());
           }
           break;
+
         case 9:
-          // if (line != b.GetbBishop().GetBoard().to_string()) {
-          //   cout << "Black Bishop moves are incorrect for FEN: " << FEN << endl;
-          //   cout << "File bitset: " << line << endl << "Board Bitset: " << b.GetbBishop().GetBoard().to_string() << endl;
-          // }
+          if (line != b.bBishopMoves().to_string()) {
+            cout << "Black Bishop moves are incorrect for FEN: " << FEN << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.bBishopMoves() << endl;
+            PrintBitSet(b.bBishopMoves());
+          }
           break;
+
         case 10:
-          // if (line != b.GetbRook().GetBoard().to_string()) {
-          //   cout << "Black Rook moves are incorrect for FEN: " << FEN << endl;
-          //   cout << "File bitset: " << line << endl << "Board Bitset: " << b.GetbRook().GetBoard().to_string() << endl;
-          // }
+          if (line != b.bRookMoves().to_string()) {
+            cout << "Black Rook moves are incorrect for FEN: " << FEN << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.bRookMoves() << endl;
+            PrintBitSet(b.bRookMoves());
+          }
           break;
+
         case 11:
-          // if (line != b.GetbQueen().GetBoard().to_string()) {
-          //   cout << "Black Queen moves are incorrect for FEN: " << FEN << endl;
-          //   cout << "File bitset: " << line << endl << "Board Bitset: " << b.GetbQueen().GetBoard().to_string() << endl;
-          // }
+          if (line != b.bQueenMoves().to_string()) {
+            cout << "Black Queen moves are incorrect for FEN: " << FEN << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.bQueenMoves() << endl;
+            PrintBitSet(b.bQueenMoves());
+          }
           break;
+
         case 12:
           if (line != b.bKingMoves().to_string()) {
             cout << "Black King moves are incorrect for FEN: " << FEN << endl;
-            cout << "File bitset:  " << line << endl << "Board Bitset: " << b.bKingMoves().to_string() << endl;
+            cout << "file bitset:  " << line << endl << "board Bitset: " << b.bKingMoves() << endl;
             PrintBitSet(b.bKingMoves());
           }
           linenum=-1;
           break;
-
-      }
+        }
       linenum+=1;
     }
     cout<<"done"<<endl;
@@ -110,6 +122,5 @@ int main() {
   } else {
     std::cerr << "Unable to open file" << std::endl;
   }
-
   return 0;
 }
