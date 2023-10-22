6c986a6fd94c6b7dbd71938624eecb1e61b021f6
jarrett-taylor-repos <jarrett.christopher.taylor@gmail.com>
2023-10-21 19:24:17 -0500

    zobrist generate file and set defaults

---
+++ Zobrist/zobristMain.cpp
@@ -0,0 +1,12 @@
+#include "ZobristHashing.h"
+
+using namespace ZobristHashing;
+
+//need to run once to generate text file 
+int main() {
+    //need to run once to generate text file 
+    //GenerateZobristNumbers();
+
+
+    LoadZobrist();
+}
