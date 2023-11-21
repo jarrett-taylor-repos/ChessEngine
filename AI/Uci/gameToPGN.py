d901699af4d06e6188355c98fd51a97796c080ed
jarrett-taylor-repos <jarrett.christopher.taylor@gmail.com>
2023-11-21 08:59:13 -0600

    small update for refactor clean up

--- AI/Uci/gameToPGN.py
+++ AI/Uci/gameToPGN.py
@@ -1,9 +1,10 @@
 import chess
 import chess.pgn
 
-pgnf = open('AI/pgnFile.txt','w')
+#need to open from here
+pgnf = open('pgnFile.txt','w')
 
-with open('AI/simgames.txt') as f:
+with open('simgames.txt') as f:
   game = chess.pgn.Game()
   # game.setup("rnb1k2r/1pqp1ppp/p3pn2/8/1b1NP3/2N1BP2/PPPQ2PP/R3KB1R b KQkq - 0 8")
   game.setup("rnbq1rk1/ppp1bppp/4pn2/3p2B1/2PP4/2N1PN2/PP3PPP/R2QKB1R b KQ - 0 6")
